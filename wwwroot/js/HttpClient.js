6457aedc3d9b60ce4caefc499e9468be109165ac
Alessandro Gomes Cunha <50428053+agomesc@users.noreply.github.com>
2022-05-01 10:02:25 -0300

    ajustes

--- wwwroot/js/HttpClient.js
+++ wwwroot/js/HttpClient.js
@@ -1,17 +1,17 @@
-function HttpClient(url) {
+function HttpClient(uri) {
     return {
         async get(id) {
-            return await fetch(`${url}/${id}`, {
+            return await fetch(`${uri}/${id}`, {
                 method: 'GET',
             }).then(this.handleErrorsAsync);
         },
         async list() {
-            return await fetch(url, {
+            return await fetch(uri, {
                 method: 'GET'
             }).then(this.handleErrorsAsync);
         },
         async post(model = {}) {
-            return await fetch(url, {
+            return await fetch(uri, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
@@ -21,7 +21,7 @@ function HttpClient(url) {
             }).then(this.handleErrorsAsync);
         },
         async put(id, model = {}) {
-            return await fetch(`${url}/${id}`, {
+            return await fetch(`${uri}/${id}`, {
                 method: 'PUT',
                 headers: {
                     'Accept': 'application/json',
@@ -31,7 +31,7 @@ function HttpClient(url) {
             });
         },
         async delete(id) {
-            return await fetch(`${url}/${id}`, {
+            return await fetch(`${uri}/${id}`, {
                 method: 'DELETE'
             });
         },
